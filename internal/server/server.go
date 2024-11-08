d7304ba75b256978fe6ffe0547905cc149e329a2
nnhuyhoang <nnhuyhoang97@gmail.com>
2024-11-08 14:18:50 +0700

    feat: add btc rpc interface

--- internal/server/server.go
+++ internal/server/server.go
@@ -1,6 +1,7 @@
 package server
 
 import (
+	"github.com/dwarvesf/icy-backend/internal/btcrpc"
 	"github.com/dwarvesf/icy-backend/internal/oracle"
 	pgstore "github.com/dwarvesf/icy-backend/internal/store/postgres"
 	"github.com/dwarvesf/icy-backend/internal/transport/http"
@@ -13,7 +14,8 @@ func Init() {
 	logger := logger.New(appConfig.Environment)
 
 	_ = pgstore.New(appConfig, logger)
-	oracle := oracle.New(appConfig, logger)
+	btcRpc := btcrpc.New(appConfig, logger)
+	oracle := oracle.New(appConfig, logger, btcRpc)
 
 	httpServer := http.NewHttpServer(appConfig, logger, oracle)
 
