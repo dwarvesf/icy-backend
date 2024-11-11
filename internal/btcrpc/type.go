73d98907e8af94a758726fc0656c1cc3564278cc
nnhuyhoang <nnhuyhoang97@gmail.com>
2024-11-11 13:50:55 +0700

    feat: add comment

--- internal/btcrpc/type.go
+++ internal/btcrpc/type.go
@@ -1,5 +1,6 @@
 package btcrpc
 
+// ChainStats represents the statistics of the blockchain referring to the transactions that have been committed to the blockchain.
 type ChainStats struct {
 	FundedTxoCount int `json:"funded_txo_count"`
 	FundedTxoSum   int `json:"funded_txo_sum"`
@@ -8,6 +9,8 @@ type ChainStats struct {
 	TxCount        int `json:"tx_count"`
 }
 
+// MempoolStats represents memory pool referring to the transactions that is in the memory
+// of the node but has not been committed to the blockchain in the block yet.
 type MempoolStats struct {
 	FundedTxoCount int `json:"funded_txo_count"`
 	FundedTxoSum   int `json:"funded_txo_sum"`
